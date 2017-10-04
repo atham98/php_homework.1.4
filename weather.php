2199df94a700b28b9a38a363e3015004a62d109f
Adham Nasrullaev <1998atham@gmail.com>
2017-10-04 19:17:48 +0500

    weather.php

---
+++ weather.php
@@ -0,0 +1,57 @@
+<?php 
+
+$link_weather = 'http://api.openweathermap.org/data/2.5/weather?id=1497917&appid=de649efb6c4dc96d792106d010e18149';
+$response = file_get_contents($link_weather);
+$weather =  json_decode($response, true);
+
+ ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style>
+		dl {
+			display: table-row;
+		}
+		dt,dd {
+			display: table-cell;
+			padding: 5px 10px;
+			font-size: 25px;
+		}
+	</style>
+</head>
+<body>
+	<h1>Погода Нефтеюганска</h1>
+
+			<dl>
+				<dt>Name</dt>
+				<dd><?=$weather['name'] ?></dd>
+			</dl>
+			<dl>
+				<dt>Weather</dt>
+				<dd><?=$weather['weather'][0]['main'] ?></dd>
+			</dl>
+			<dl>
+				<dt>Description</dt>
+				<dd><?=$weather['weather'][0]['description'] ?></dd>
+			</dl>
+			<dl>
+				<dt>Temperature</dt>
+				<dd><?=$weather['main']['temp'] ?></dd>
+			</dl>
+			<dl>
+				<dt>Visibility</dt>
+				<dd><?=$weather['visibility'] ?></dd>
+			</dl>
+			<dl>
+				<dt>Pressure</dt>
+				<dd><?=$weather['main']['pressure'] ?></dd>
+			</dl>
+
+</body>
+</html>
+
+
+
